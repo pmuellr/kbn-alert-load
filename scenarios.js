0e00dfaf8a1d06ffcbb13c4c3204a64890e3da34
Patrick Mueller <pmuellr@gmail.com>
2020-11-06 02:25:26 -0500

    scroll to get event log docs

--- scenarios.js
+++ scenarios.js
@@ -11,13 +11,14 @@ module.exports = [
   scenarioJustAlerts(500),
   scenarioJustAlerts(1000),
   scenarioJustAlerts(2000),
+  scenarioJustAlerts(4000),
 ]
 
 /** @type { (alerts: number) => Scenario } */
 function scenarioJustAlerts(alerts) {
   return {
     id: `JustAlerts-${alerts}`,
-    minutes: 10,
+    minutes: 5,
     deployments: [
       { es: '1x1', kb: '1x1' },
       { es: '1x8', kb: '2x8' },
